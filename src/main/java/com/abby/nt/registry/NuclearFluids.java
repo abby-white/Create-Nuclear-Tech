6d8573bd624f7ce6a569242490fe7c923c11febf
abby-white <145313911+abby-white@users.noreply.github.com>
2023-12-26 13:35:28 -0800

    wip Steam textures

--- src/main/java/com/abby/nt/registry/NuclearFluids.java
+++ src/main/java/com/abby/nt/registry/NuclearFluids.java
@@ -1,65 +1,63 @@
 package com.abby.nt.registry;
 
-import com.abby.nt.CreateNuclear;
 import com.simibubi.create.AllFluids;
+import com.simibubi.create.AllTags;
+import com.simibubi.create.foundation.utility.Color;
 import com.tterrag.registrate.builders.FluidBuilder;
 import com.tterrag.registrate.util.entry.FluidEntry;
-import com.tterrag.registrate.util.nullness.NonNullFunction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
-import net.minecraftforge.fluids.ForgeFlowingFluid.*;
+import org.joml.Vector3f;
+
+import java.util.function.Supplier;
 
 import static com.abby.nt.CreateNuclear.REGISTRATE;
+import static com.abby.nt.CreateNuclear.asResource;
 
 public class NuclearFluids {
 
     public static void init(){};
 
-    public static final FluidEntry<Flowing> STEAM = register("steam", NoColorFluidAttributes::new);
 
 
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> STEAM =
+            REGISTRATE.fluid("steam",asResource("block/water_still"), asResource("block/water_still"),
+                            SolidRenderedPlaceableFluidType.create(0xCCCCCC,
+                                    () -> 1f / 8f * 1))
+                    .lang("Steam")
+                    .properties(b -> b.viscosity(2000)
+                            .density(-100))
+                    .fluidProperties(p -> p.levelDecreasePerBlock(2)
+                            .tickRate(25)
+                            .slopeFindDistance(3)
+                            .explosionResistance(100f))
+                    .source(ForgeFlowingFluid.Source::new) // TODO: remove when Registrate fixes FluidBuilder
+                    .bucket()
+                    .tag(AllTags.forgeItemTag("buckets/steam"))
+                    .build()
+                    .register();
 
-    @SuppressWarnings("SameParameterValue")
-    private static FluidEntry<Flowing> register(String name, String resourceLocation, FluidBuilder.FluidTypeFactory typeFactory,
-                                                                  NonNullFunction<ForgeFlowingFluid.Properties, ? extends ForgeFlowingFluid> customSource,
-                                                                  int viscosity, int density, int levelDecreasePerBlock,
-                                                                  int tickRate, int slopeFindDistance, float explosionResistance) {
 
-        return REGISTRATE.fluid(name,
-                        CreateNuclear.asResource("fluid/%s_still".formatted(resourceLocation)),
-                        CreateNuclear.asResource("fluid/%s_flow".formatted(resourceLocation)),
-                        typeFactory)
-                .lang(Character.toUpperCase(name.charAt(0)) + name.substring(1))
-                .properties(b -> b.viscosity(viscosity).density(density))
-                .fluidProperties(p ->
-                        p.levelDecreasePerBlock(levelDecreasePerBlock)
-                                .tickRate(tickRate)
-                                .slopeFindDistance(slopeFindDistance)
-                                .explosionResistance(explosionResistance)
-                )
-                .source(customSource)
-                .bucket()
-                .build()
-                .register();
-    }
-    private static FluidEntry<Flowing> register(String name, String resourceLocation, FluidBuilder.FluidTypeFactory typeFactory) {
-        return register(name, resourceLocation, typeFactory, ForgeFlowingFluid.Source::new);
-    }
+    private static class SolidRenderedPlaceableFluidType extends AllFluids.TintedFluidType {
 
-    private static FluidEntry<Flowing> register(String name, String resourceLocation, FluidBuilder.FluidTypeFactory typeFactory,
-                                                NonNullFunction<ForgeFlowingFluid.Properties, ? extends ForgeFlowingFluid> customSource) {
-        return register(name, resourceLocation, typeFactory, customSource, 2000, 0, 2, 25, 3, 100f);
-    }
+        private Vector3f fogColor;
+        private Supplier<Float> fogDistance;
 
-    private static FluidEntry<Flowing> register(String name, FluidBuilder.FluidTypeFactory typeFactory) {
-        return register(name, name, typeFactory);
-    }
-    private static class NoColorFluidAttributes extends AllFluids.TintedFluidType {
-        public NoColorFluidAttributes(Properties properties, ResourceLocation stillTexture, ResourceLocation flowingTexture) {
+        public static FluidBuilder.FluidTypeFactory create(int fogColor, Supplier<Float> fogDistance) {
+            return (p, s, f) -> {
+                SolidRenderedPlaceableFluidType fluidType = new SolidRenderedPlaceableFluidType(p, s, f);
+                fluidType.fogColor = new Color(fogColor, false).asVectorF();
+                fluidType.fogDistance = fogDistance;
+                return fluidType;
+            };
+        }
+
+        private SolidRenderedPlaceableFluidType(Properties properties, ResourceLocation stillTexture,
+                                                ResourceLocation flowingTexture) {
             super(properties, stillTexture, flowingTexture);
         }
 
@@ -68,9 +66,25 @@ public class NuclearFluids {
             return NO_TINT;
         }
 
+        /*
+         * Removing alpha from tint prevents optifine from forcibly applying biome
+         * colors to modded fluids (this workaround only works for fluids in the solid
+         * render layer)
+         */
         @Override
         public int getTintColor(FluidState state, BlockAndTintGetter world, BlockPos pos) {
             return 0x00ffffff;
         }
+
+        @Override
+        protected Vector3f getCustomFogColor() {
+            return fogColor;
+        }
+
+        @Override
+        protected float getFogDistanceModifier() {
+            return fogDistance.get();
+        }
+
     }
 }
